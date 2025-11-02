74e0921caad87de66c0c5ad72751e0dc094ea6ed
duong-cmd <duongncys@gmail.com>
2025-11-02 22:14:55 +0700

    Update DocGiaThuong.cpp

--- src/DocGiaThuong.cpp
+++ src/DocGiaThuong.cpp
@@ -1,28 +1,16 @@
 #include "DocGiaThuong.h"
 #include <iostream>
-
 using namespace std;
-
 DocGiaThuong::DocGiaThuong() : DocGia(), gioiHanMuon(5) {}
 
 DocGiaThuong::DocGiaThuong(string ten, string maSo, int soSachDaMuon, int gioiHanMuon)
     : DocGia(ten, maSo, soSachDaMuon), gioiHanMuon(gioiHanMuon) {}
 
-int DocGiaThuong::getGioiHanMuon() const {
-    return gioiHanMuon;
-}
-
-void DocGiaThuong::setGioiHanMuon(int gioiHan) {
-    gioiHanMuon = gioiHan;
-}
+int DocGiaThuong::getGioiHanMuon() const { return gioiHanMuon; }
+void DocGiaThuong::setGioiHanMuon(int gioiHan) { gioiHanMuon = gioiHan; }
 
 void DocGiaThuong::hienThiThongTin() const {
-    cout << "=== THONG TIN DOC GIA THUONG ===" << endl;
-    cout << "Ten: " << getTen() << endl;
-    cout << "Ma so: " << getMaSo() << endl;
-    cout << "So sach da muon: " << getSoSachDaMuon() << endl;
+    std::cout << "=== THONG TIN DOC GIA THUONG ===" << std::endl;
     cout << "Gioi han muon: " << gioiHanMuon << " quyen" << endl;
     cout << "===============================" << endl;
 }
-
-
