b93cd7988f98cdf8a522df0f7cbdd6b70681befa
duong-cmd <duongncys@gmail.com>
2025-11-02 22:13:01 +0700

    Update HoiVien.cpp

--- src/HoiVien.cpp
+++ src/HoiVien.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 HoiVien::HoiVien() : DocGia(), thoiGianMuonMax(30), tileGiamGia(0.1), 
                      ngayDangKy(""), ngayHetHan("") {}
+
 HoiVien::HoiVien(string ten, string maSo, int soSachDaMuon, 
                  int thoiGianMuonMax, double tileGiamGia, 
                  string ngayDangKy, string ngayHetHan)
@@ -12,6 +13,7 @@ HoiVien::HoiVien(string ten, string maSo, int soSachDaMuon,
       tileGiamGia(tileGiamGia),
       ngayDangKy(ngayDangKy), 
       ngayHetHan(ngayHetHan) {}
+
 int HoiVien::getThoiGianMuonMax() const {
     return thoiGianMuonMax;
 }
@@ -38,13 +40,9 @@ void HoiVien::setNgayHetHan(string ngay) {
 }
 void HoiVien::hienThiThongTin() const {
     cout << "=== THONG TIN HOI VIEN ===" << endl;
-    cout << "Ten: " << getTen() << endl;
-    cout << "Ma so: " << getMaSo() << endl;
-    cout << "So sach da muon: " << getSoSachDaMuon() << endl;
     cout << "Thoi gian muon toi da: " << thoiGianMuonMax << " ngay" << endl;
     cout << "Ti le giam gia: " << (tileGiamGia * 100) << "%" << endl;
     cout << "Ngay dang ky: " << ngayDangKy << endl;
     cout << "Ngay het han: " << ngayHetHan << endl;
     cout << "==========================" << endl;
 }
-
